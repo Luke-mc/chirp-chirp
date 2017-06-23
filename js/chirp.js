eea246144cf0b05b1488ce4f84d2aaba30deaf7f
Luke-mc <luke_mcglynn@yahoo.com>
2017-06-22 18:19:21 -1000

    fixed spacing

--- js/chirp.js
+++ js/chirp.js
@@ -1,20 +1,11 @@
 
 function chirp(n){
   // FIX ME
-
   if(n === 1){
-
     return "chirp";
-
   }
-
-
   return "chirp " + chirp(--n);
-
-
 };
-
-
 
 $(document).ready(function(){
   $("#result").html(chirp(3));
